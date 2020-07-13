f032dd891159f8efc8ca8e9772914368682b2f0e
ssudemirci <selcukqa@gmail.com>
2020-07-13 18:46:20 -0400

    Day01 Class

---
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -0,0 +1,27 @@
+package com.cybertek.step_definitions;
+
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class GoogleStepDefinitions {
+    @Given("user is on the google search page")
+    public void user_is_on_the_google_search_page() {
+
+        Assert.fail("Unpurpose failing here!!!");
+        Driver.getDriver().get("https://www.google.com");
+
+
+    }
+
+    @Then("User should see title is Google")
+    public void user_should_see_title_is_google() {
+        String actualTitle=Driver.getDriver().getTitle();
+        String expectedTitle="Google";
+
+
+        Assert.assertEquals("Actual title does not match expected title!",actualTitle,expectedTitle);
+
+    }
+}
---
+++ target/test-classes/features/Search.feature
@@ -0,0 +1,8 @@
+Feature: Search
+
+  Agile Story:As a user, I should be able to search when I am on Google search
+
+  Scenario:Google default title verification
+    Given user is on the google search page
+    Then User should see title is Google
+
