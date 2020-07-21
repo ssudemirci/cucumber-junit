8531d04105d50f4e1e7655f653784019a0c18686
ssudemirci <selcukqa@gmail.com>
2020-07-21 12:02:17 -0400

    Day17 Class

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-report.html",
-        "json:target/cucumber.json"
+        "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
         },
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
